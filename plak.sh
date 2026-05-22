9921b8bb21c87dc1efce8f734dea2eb8cc63ca03
Oscar Hugo Paz <oscarhugopazmariscal@gmail.com>
2026-05-22 18:32:30 -0500

    Release 0.4.27

--- plak.sh
+++ plak.sh
@@ -6,7 +6,7 @@
 set -euo pipefail
 
 PLAK_NAME="plak"
-PLAK_VERSION="0.4.26"
+PLAK_VERSION="0.4.27"
 PLAK_HOME="${PLAK_HOME:-$HOME/.plak}"
 PLAK_SSH_CONFIG="${PLAK_SSH_CONFIG:-$HOME/.ssh/config}"
 PLAK_HOSTS_FILE="${PLAK_HOSTS_FILE:-/etc/hosts}"
